f7cedfc1609ac77a4d0fd7dff2eb43234844296a
Matthew Buchanan <matt@matthews-air.home>
2018-04-20 23:00:28 -0400

    OMDB API key

--- liri.js
+++ liri.js
@@ -2,3 +2,5 @@ require("dotenv").config();
 var keys = require('keys.js');
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+queryUrl = `http://www.omdbapi.com/?i=tt3896198&apikey=48075365`;
